7d9578cb4019d171c9900103b7a5ff9a0a9a56e0
Geenie <1270851085@qq.com>
2018-12-16 00:17:01 +0800

    delete commit

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -1,27 +1,10 @@
 import React from 'react'
-import { Link, Route, BrowserRouter as Router } from 'react-router-dom'
-import Nav from '../nav/index'
 import './styles/index.css'
 
 export default class Home extends React.PureComponent {
     render () {
         return (
-            // <Router>
-            // <div className="home">
-            //     <Link to="/nav">Modus Create</Link>
-            //     <Route path='/nav' component={Nav}/>
-            // </div>
-            // </Router>
-
-            <Router>
-            <div className="home">
-                <Link to="/nav">Modus Create</Link>
-                <Route path='/nav' component={Nav}/>
-                {/* <Route path='/:id' component={Child} /> */}
-            </div>
-            </Router>
-            
-            // <div>home页面</div>
+            <div>home页面</div>
         )
     }
 }
